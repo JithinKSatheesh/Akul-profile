074ec6efbcd921d7e4c917869cb46f04a8e8a489
Jithin K Satheesh <37372507+JithinKSatheesh@users.noreply.github.com>
2020-03-17 20:25:09 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,194 @@
+let data = {
+    "Home": {
+        "title": "Hi I am Akul Santhosh",
+        "image": "<img src='https://share.getcloudapp.com/items/DOu8D5YZ/download'>",
+        // "image": "<img src='image/profile.jpeg'>",
+        "description": "I am a final year B.Tech student in the Division of Computer Science at School of Engineering CUSAT. I have cofounded a startup 'Trebuchet' that tries to bridge the gap between customer and online shopping using Augmented Reality and Machine Learning. The startup is currently incubated at CITTIC and has recieved funding through Idea Day Grant by Kerala Startup Mission.<br><br>Apart from this I enjoy organising tech events and hackathons, I have succesfully founded the hackathon 'Make-a-Ton', which has now grown into became one of India's best college hackathons.",
+        "address": "102B Malacheri Veedu<br>Bodhi Nagar Palathara<br>Thattamala Kollam<br>Kerala IN 691020",
+        "social": {
+            "Email : ": "<a href='mailto:akul753@gmail.com.com?Subject=Hello' target='_top'>akul753@gmail.com</a>",
+            "Phone No : ": "<a href='tel:+917012094615'>+91 7012094615</a>",
+            "LinkedIn : ": "<a href='https://www.linkedin.com/in/akulsanthosh/'>akulsanthosh</a>",
+            "Twitter : ": "<a href='https://twitter.com/akul753'>@akul753</a>",
+            // "Quora : ": "<a href='https://www.quora.com/profile/Akul-Santhosh'>Q&A AkulS</a>"
+        }
+    },
+    "CV": {
+        "title": "To download offline version click <a href='./resume.pdf'>here</a>",
+        "list": {
+            "Education": {
+                "cusat": {
+                    "title": "Cochin University of Science and Technology",
+                    "subtitle": "Bachelor of Technology in Computer Science",
+                    "date": "August 2016 - May 2020",
+                    "description": "<b>CGPA - 9.0 /10, Ranked 7th in class.</b> <br>Core Courses:- Operating System, Algorithms, Data Structures, Object Oriented Programming Language, Computer Architecture and Organisation, Principles of Programming Languages, Data Mining, Graphics, Automata Languages."
+                }
+            },
+            "Startup": {
+                "trebuchet": {
+                    "title": "Trebuchet",
+                    "subtitle": "Cofounder",
+                    "date": "September 2018 - Present",
+                    "description": "Trebuchet is a distruptive startup focused on bridging the gap between customer and online shopping using Augmented Reality (AR) and Machine Learning (ML). Currently a working prototype of an iOS based AR application that uses semantic segmentation to capture the orientation of user's foot, ARKit and CoreML to augment and run the ML models. This system has now been successfully ported into android and a kiosk has been developed using Qualcomm DragonBoard. The startup has won the prestigious Idea Day Grant and has also secured 3 buyers for the kiosk."
+                }
+            },
+            "Internship": {
+                "wipro": {
+                    "title": "Wipro Limited",
+                    "subtitle": "Research Intern for Industrial IoT Lab",
+                    "date": "June 2019 - July 2019",
+                    "description": "The work involved collaborating with senior research team to formulate a structure for integrating IoT devices into a semantic system. The semantic system consists of metadata that defines the functionality of IoT devices, system to autoregister new devices and designed JSON-LD based data structure for efficient distribution over the network. The accessibility of the system was improved by incorporating conversational methods of interaction."
+                },
+                "iraa": {
+                    "title": "International Relations and Academic Admission CUSAT",
+                    "subtitle": "Intern for Common Admission Test Counselling",
+                    "date": "April 2019 - May 2019",
+                    "description": "The admission at Cochin University is based on the rank secured in the common admission test (CAT). Once the ranklist is published the admission process involves live counselling with the students. The essential software for counselling process including biometrics, live waitlist and verification is done by IRAA. The system previously designed by the National Informatics Centre (NIA) was redesigned inhouse using Django framework and MySQL database. The system passed security audits instituted by the University Grants Commisions, Govt of India."
+                },
+                "hal": {
+                    "title": "Hindustan Aeronautics Limited",
+                    "subtitle": "Intern for Information Technology Division",
+                    "date": "May 2018 - June 2018",
+                    "description": "Built system for condition monitoring of helicopters using Principal Component Analysis (PCA) and then by calculating Mahalanobis distance (MD). The system attained 93% accuracy in prediction which reduced time to check by 2x. Understood the basic aerodynamics of helicopters, the construction techniques, the different divisions involved and the realtime OS used by them."
+                }
+            },
+            "Projects": {
+                "convai": {
+                    "title": "Convai - Conversational OS",
+                    "subtitle": "Final Year Project",
+                    "date": "2020",
+                    "description": "A conversational web based OS that works on simple voice commands. The OS modules include UI building developer tools, schema to define data and ui interaction, inbuild handlers for small talk, smart intent mapping to select between multiple application, and graphical UI support. This project was designed such that it would be natively accessible for the visual impaired.<br><br><b>Demo : </b>Work in Progress"
+                },
+                "semantic": {
+                    "title": "Semantic Lab",
+                    "subtitle": "Wipro IoT Lab",
+                    "date": "2019",
+                    "description": "A semantic system that can control the IoT sensors, autoregister and provide internet based command line interface. The system is natively conversational and can be integrated with popular voice agents like Google Home, Amazon Alexa etc.<br><br><b>Demo : </b><a href='https://www.youtube.com/watch?v=4MyNWBz7s_0'>Video</a>"
+                },
+                "iris": {
+                    "title": "Iris - Conversational email generator",
+                    "subtitle": "AngelHack Hackathon Bangalore",
+                    "date": "2019",
+                    "description": "A dialogflow based conversational system that generates email based on user responses. The system is customisable and has a  NLP system to generate queries inorder to understand user intention.<br><br><b>Demo : </b><a href='#'>Video</a>"
+                },
+                "arware": {
+                    "title": "ARware",
+                    "subtitle": "Minor Project",
+                    "date": "2019",
+                    "description": "An iOS application to virtually try on shoes in real time, combining AR and ML. The dataset was created using various foot semantic labelled images to train a semantic segmentation model for detection and understanding of foot orientation. The 3D model of the shoe was then augmented using ARKit. Real time augmentation was achieved by optimising the pipeline including dropping frames based on analysing change in camera feed.<br><br><b>Demo : </b><a href='#'>Video</a>"
+                },
+                "PillD": {
+                    "title": "PillD - Intelligent Pill Dispenser",
+                    "subtitle": "Wipro Industrail IoT Hackathon",
+                    "date": "2019",
+                    "description": "An intelligent pill dispensor with alexa, a deep learning model analyses health data, makes intelligent predictions and dispenses pills. The system works using live data from various health sensors, prescriptions and historical data models.<br><br><b>Demo : </b><a href='#'>Video</a>"
+                },
+                "NoQStore": {
+                    "title": "NoQStore - Cost effective Amazon Go",
+                    "subtitle": "Techstars Startup Weekend",
+                    "date": "2018",
+                    "description": "Created a model of Amazon Go with cheaper components and voice guided systems, load cells and mobile cameras where used to create the functionality. Created during Techstars Startup Weekend hackathon where this project won most innovative product award.<br><br><b>Github : </b><a href='https://github.com/akulsanthosh/noQ'>Repository</a>"
+                }
+            },
+            "Leadership/Volunteering": {
+                "placement": {
+                    "title": "Placement Coordinator",
+                    "subtitle": "Central Placement Cell CUSAT",
+                    "date": "2019 - 2020",
+                    "description": "Organized placement activities, job fairs, internship drives and coordinated with companies. Actively involoved in expanding industrial participation in placements."
+                },
+                "aces": {
+                    "title": "Secretary",
+                    "subtitle": "Association of Computer Engineering Students CUSAT",
+                    "date": "2019 - 2020",
+                    "description": "Represented CS students in addressing general grievances and academic matters. To interact with companies to organise industrial visits, intership drives and workshops. Mentoring students to participate and organise in hackathons, startup initiatives and tech events. Conducted C++ programming classes as part of Session@4 program."
+                },
+                "makeaton": {
+                    "title": "Founder and Organiser",
+                    "subtitle": "Make-a-Ton Hackathon",
+                    "date": "2018 - 2020",
+                    "description": "Coordinated and planned entire event flow including sponsorships, partnerships, fund management, outreach, technical management, media interaction, selecting judges, themes and activities."
+                },
+                "aws": {
+                    "title": "Social Media Manager and Guest Coordinator",
+                    "subtitle": "AWS Community Day",
+                    "date": "2019",
+                    "description": ""
+                },
+                "flood": {
+                    "title": "Volunteer",
+                    "subtitle": "Kerala Flood Relief",
+                    "date": "2018",
+                    "description": "Coordinated logistics in transporting and managing 5000 students to nearly 10000 affected homes. Special recognition from the Government of Kerala and one among the 14 students from Kerala to be awarded for the same."
+                }
+            },
+            "Achievments/Certifications": {
+                "coursera1": {
+                    "title": "How Google does Machine Learning",
+                    "subtitle": "Coursera 6QM69Q2M9VPM",
+                    "date": "2019",
+                    "description": ""
+                },
+                "coursera2": {
+                    "title": "Intro to Tensor Flow",
+                    "subtitle": "Coursera Q35U2Q8X7S3Z",
+                    "date": "2019",
+                    "description": ""
+                },
+                "coursera3": {
+                    "title": "Launching into Machine Learning",
+                    "subtitle": "Coursera ZE5KB7R4L8DR",
+                    "date": "2019",
+                    "description": ""
+                },
+                "nptel2": {
+                    "title": "Design and Analysis of Algorithms",
+                    "subtitle": "National Program for Technology Enchanced Learning",
+                    "date": "2019",
+                    "description": ""
+                },
+                "ibm": {
+                    "title": "Robotic Behavior Driven by Machine Learning",
+                    "subtitle": "IBM Research",
+                    "date": "2019",
+                    "description": ""
+                },
+                "nptel1": {
+                    "title": "Programming in C++",
+                    "subtitle": "National Program for Technology Enchanced Learning",
+                    "date": "2018",
+                    "description": "<b>Top 2% in final examination</b>"
+                }
+            }
+        }
+    }
+    // "Moments": {
+    //     "image1": {
+    //         "title": "my photo",
+    //         "url": "https://share.getcloudapp.com/items/DOu8D5YZ/download"
+    //     },
+    //     "image2": {
+    //         "title": "my photo",
+    //         "url": "https://share.getcloudapp.com/items/DOu8D5YZ/download"
+    //     },
+    //     "image5": {
+    //         "title": "",
+    //         "url": ""
+    //     },
+    //     "image6": {
+    //         "title": "",
+    //         "url": ""
+    //     },
+    //     "image7": {
+    //         "title": "",
+    //         "url": ""
+    //     },
+    //     "image8": {
+    //         "title": "",
+    //         "url": ""
+    //     }
+    // }
+    // "Blog": {
+    //     "title": "Check back soon"
+    // }
+}
